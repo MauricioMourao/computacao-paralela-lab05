5b6d0878bb23c84c623a27d4ece5d52cf8324029
MauricioMourao <79863867+MauricioMourao@users.noreply.github.com>
2021-09-15 22:05:09 -0300

    Update exercicio_5.c

--- exercicio_5.c
+++ exercicio_5.c
@@ -3,28 +3,28 @@
 
 int main()
 {
-    int array[MAX_SIZE] = {10, 20, 30, 40, 50, 60};	//CRIA ARRAY
-    int temp1[MAX_SIZE/2];							//CRIA PRIMEIRO ARRAY SECUNDARIO
-    int temp2[MAX_SIZE/2];							//CRIA SEGUNDO ARRAY SECUNDARIO
-    int i, j, k;
+	int array[MAX_SIZE] = {10, 20, 30, 40, 50, 60};	//CRIA ARRAY
+	int temp1[MAX_SIZE/2];				//CRIA PRIMEIRO ARRAY SECUNDARIO
+	int temp2[MAX_SIZE/2];				//CRIA SEGUNDO ARRAY SECUNDARIO
+	int i, j, k;
 
 	/*
 	Aqui seria uma parte de paralelizar a primeira metade do array em um processador
-	e a segunda metade no outro, assim seria executada a tarefa de multiplicação na
+	e a segunda metade no outro, assim seria executada a tarefa de multiplicaÃ§Ã£o na
 	metade do tempo.
 	*/
 
 	for(i=0; i< MAX_SIZE/2; i++){	//PRIMEIRO PROCESSADOR MULTIPLICA A PRIMEIRA METADE E SALVA NO ARRAY
 		array[i] = temp1[i] * 4;
 	}
-	
+
 	for(j=0; j< MAX_SIZE/2; j++){	//SEGUNDO PROCESSADOR MULTIPLICA A SEGUNDA METADE E SALVA NO ARRAY
 		array[j+3] = temp2[j] * 4;
 	}
-	
+
 	/*
-	Depois que cada processador sobrescrever a resposta em cada espaço do array ele 
-	será impresso na tela.
+	Depois que cada processador sobrescrever a resposta em cada espaÃ§o do array ele 
+	serÃ¡ impresso na tela.
 	*/
 
 	for(k=0; k < MAX_SIZE; k++){	//IMPRIME O ARRAY
@@ -33,4 +33,3 @@ int main()
 
     return 0;
 }
-
